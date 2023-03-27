8dffd1769db329c4e2a35b7bed4d228690b2c637
serdyuksb1982 <serdyuksb@gmail.com>
2023-03-27 22:16:21 +0300

    Factory method for create bean...

--- src/main/java/com/example/spring/lesson_1/ApplicationRunner.java
+++ src/main/java/com/example/spring/lesson_1/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package com.example.spring.lesson_1;
 
+import com.example.spring.lesson_1.database.repository.CompanyRepository;
 import com.example.spring.lesson_1.database.rpool.ConnectionPool;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,5 +20,7 @@ public class ApplicationRunner {
         //Map<String, Object>
         var connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println((connectionPool));
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
--- src/test/java/com/example/spring/SpringStarterApplicationTests.java
+++ src/test/java/com/example/spring/SpringStarterApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.spring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringStarterApplicationTests {
 
     @Test
