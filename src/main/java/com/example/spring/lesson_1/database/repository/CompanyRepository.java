62f07b5c083a77735483d99aa4c67090a156e54e
serdyuksb1982 <serdyuksb@gmail.com>
2023-04-05 17:14:55 +0300

    BeanPostProcessor.beforeInitialization...

--- src/main/java/com/example/spring/lesson_1/database/repository/CompanyRepository.java
+++ src/main/java/com/example/spring/lesson_1/database/repository/CompanyRepository.java
@@ -1,15 +1,17 @@
 package com.example.spring.lesson_1.database.repository;
 
+import com.example.spring.lesson_1.bpp.InjectBean;
 import com.example.spring.lesson_1.database.rpool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
+    /*public CompanyRepository(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 
     public static CompanyRepository of(ConnectionPool connectionPool) {
         return new CompanyRepository(connectionPool);
-    }
+    }*/
 }
